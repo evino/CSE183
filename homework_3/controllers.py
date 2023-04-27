3b9dcf674d4bc6b0b0092bc2815095bfbfd789a4
Evin Odisho <evinodisho@gmail.com>
2023-04-26 21:03:39 -0700

    Completed, and uploaded zip file

--- homework_3/controllers.py
+++ homework_3/controllers.py
@@ -35,14 +35,14 @@ from . common import db, session, T, cache, auth, signed_url
 url_signer = URLSigner(session)
 
 @action('index')
-@action.uses('index.html', db, auth)
+@action.uses('index.html', db, auth, url_signer)
 def index():
     ## TODO: Show to each logged in user the birds they have seen with their count.
     # The table must have an edit button to edit a row, and also, a +1 button to increase the count
     # by 1 (this needs to be protected by a signed URL).
     # On top of the table there is a button to insert a new bird.
     rows = db(db.bird.user_email == get_user_email()).select()
-    return dict(rows=rows)
+    return dict(rows=rows, url_signer=url_signer)
 
 
 @action('add', method=["GET", "POST"])
@@ -67,16 +67,34 @@ def edit(bird_id=None):
     # Edit form
     form = Form(db.bird, record=b, deletable=False, csrf_session=session, formstyle=FormStyleBulma)
     if form.accepted:
-        # Update already ahppened
+        # Update already happened
         redirect(URL('index'))
     return dict(form=form)
 
-# This is an example only, to be used as inspiration for your code to increment the bird count.
-# Note that the bird_id parameter ...
-@action('capitalize/<bird_id:int>') # the :int means: please convert this to an int.
-@action.uses(db, auth.user, url_signer.verify())
-# ... has to match the bird_id parameter of the Python function here.
-def capitalize(bird_id=None):
+# Increment endpoint
+@action('inc/<bird_id:int>')
+@action.uses(db, session, url_signer.verify())
+def inc(bird_id=None):
     assert bird_id is not None
     bird = db.bird[bird_id]
-    db(db.bird.id == bird_id).update(bird_name=bird.bird_name.capitalize())
+    db(db.bird.id == bird_id).update(n_sightings=(int(bird.n_sightings) + 1))
+    redirect(URL('index'))
+    return dict()
+
+
+@action('del/<bird_id:int>')
+@action.uses(db, session, url_signer.verify())
+def inc(bird_id=None):
+    assert bird_id is not None
+    db(db.bird.id == bird_id).delete()
+    redirect(URL('index'))
+
+# This is an example only, to be used as inspiration for your code to increment the bird count.
+# Note that the bird_id parameter ...
+# @action('capitalize/<bird_id:int>') # the :int means: please convert this to an int.
+# @action.uses(db, auth.user, url_signer.verify())
+# # ... has to match the bird_id parameter of the Python function here.
+# def capitalize(bird_id=None):
+#     assert bird_id is not None
+#     bird = db.bird[bird_id]
+#     db(db.bird.id == bird_id).update(bird_name=bird.bird_name.capitalize())
